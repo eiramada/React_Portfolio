bf17680b177ffc303d6f505bd4891abeed4346d7
eiramada <113309511+eiramada@users.noreply.github.com>
2024-05-24 18:32:59 +0300

    add & delete employee, validation

--- bakery-shop/src/pages/Employees.jsx
+++ bakery-shop/src/pages/Employees.jsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { useEffect, useRef, useState } from "react";
+import { Alert, Button, Table } from "react-bootstrap";
+import validator from "validator";
 
 function Employees() {
   const employeesUrl = "https://reqres.in/api/users";
   const [employees, setEmployees] = useState([]);
+  const [message, setMessage] = useState();
+
+  const idRef = useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const avatarRef = useRef();
 
   useEffect(() => {
     fetch(employeesUrl)
@@ -12,18 +19,72 @@ function Employees() {
   }, []);
 
   const addEmployee = () => {
-    // TODO: Add validations
-    // TODO: Add an employee to the table
+    if (!validate()) return;
+    addEmployeeToTable();
+    resetValues();
   };
 
   const deleteEmployee = (index) => {
-    // TODO: Delete an employee from the table
+    employees.splice(index, 1);
+    setEmployees(employees.slice());
+  };
+
+  const validate = () => {
+    if (!idRef.current.value || !validator.isNumeric(idRef.current.value)) {
+      setMessage("ID is required and has to contain only numbers ");
+      return false;
+    }
+    if (employees.find((p) => p.id === Number(idRef.current.value))) {
+      setMessage("Id has to be unique");
+      return false;
+    }
+
+    if (
+      !nameRef.current.value ||
+      !validator.isAlpha(nameRef.current.value.replace(" ", ""))
+    ) {
+      setMessage("Name is required and has to contain only letters");
+      return false;
+    }
+
+    if (!emailRef.current.value || !validator.isEmail(emailRef.current.value)) {
+      setMessage("Email is required and must be valid");
+      return false;
+    }
+
+    if (!avatarRef.current.value) {
+      setMessage("Avatar URL is required");
+      return false;
+    }
+
+    setMessage("");
+    return true;
+  };
+
+  const addEmployeeToTable = () => {
+    const newEmployee = {
+      id: Number(idRef.current.value),
+      first_name: nameRef.current.value.split(" ")[0],
+      last_name: nameRef.current.value.split(" ")[1] || "",
+      email: emailRef.current.value,
+      avatar: avatarRef.current.value,
+    };
+    employees.push(newEmployee);
+    setEmployees(employees.slice());
+  };
+
+  const resetValues = () => {
+    idRef.current.value = "";
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+    avatarRef.current.value = "";
   };
 
   return (
     <div>
       <div className="container">
         <h2 className="mb-4">Employees</h2>
+        {message && <Alert variant="danger">{message}</Alert>}
         <Table className="table table-hover table-bordered table-sortable">
           <thead>
             <tr>
@@ -35,8 +96,8 @@ function Employees() {
             </tr>
           </thead>
           <tbody>
-            {employees.map((employee) => (
-              <tr>
+            {employees.map((employee, index) => (
+              <tr key={index}>
                 <td>{employee.id}</td>
                 <td>
                   {employee.first_name} {employee.last_name}
@@ -46,33 +107,50 @@ function Employees() {
                   <img src={employee.avatar} alt="" />
                 </td>
                 <td>
-                  <Button type="button" variant="danger">
+                  <Button
+                    onClick={() => deleteEmployee(index)}
+                    variant="danger"
+                  >
                     Delete
                   </Button>
                 </td>
               </tr>
             ))}
-
             <tr className="input-row">
               <td>
-                <input type="text" placeholder="ID" className="form-control" />
+                <input
+                  type="number"
+                  placeholder="ID"
+                  className="form-control"
+                  ref={idRef}
+                />
               </td>
               <td>
                 <input
                   type="text"
                   placeholder="Name"
                   className="form-control"
+                  ref={nameRef}
+                />
+              </td>
+              <td>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="form-control"
+                  ref={emailRef}
                 />
               </td>
               <td>
                 <input
                   type="text"
-                  placeholder="Email"
+                  placeholder="Avatar URL"
                   className="form-control"
+                  ref={avatarRef}
                 />
               </td>
               <td>
-                <Button type="submit" variant="success">
+                <Button onClick={addEmployee} type="submit" variant="success">
                   Add
                 </Button>
               </td>
